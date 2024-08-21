a1824fe3f572366c260c1bab0af71a4070756fa1
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 12:56:29 +0300

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -11,28 +11,28 @@ sudo yum install -y nodejs npm
 sudo yum install -y python3-pip
 
 # Debug: List contents of the project directory and subdirectories
-echo "Listing contents of /home/ec2-user/majaliwasocialtest"
-ls -la /home/ec2-user/majaliwasocialtest
+echo "Listing contents of /home/ec2-user/"
+ls -la /home/ec2-user/
 
-echo "Listing contents of /home/ec2-user/majaliwasocialtest/frontend (if exists)"
-ls -la /home/ec2-user/majaliwasocialtest/frontend || echo "Frontend directory not found!"
+echo "Listing contents of /home/ec2-user/frontend (if exists)"
+ls -la /home/ec2-user/frontend || echo "Frontend directory not found!"
 
-echo "Listing contents of /home/ec2-user/majaliwasocialtest/backend (if exists)"
-ls -la /home/ec2-user/majaliwasocialtest/backend || echo "Backend directory not found!"
+echo "Listing contents of /home/ec2-user/backend (if exists)"
+ls -la /home/ec2-user/backend || echo "Backend directory not found!"
 
 # Ensure the deployment directories exist
-if [ ! -d "/home/ec2-user/majaliwasocialtest/frontend" ]; then
+if [ ! -d "/home/ec2-user/frontend" ]; then
   echo "Frontend directory does not exist!"
   exit 1
 fi
 
-if [ ! -d "/home/ec2-user/majaliwasocialtest/backend" ]; then
+if [ ! -d "/home/ec2-user/backend" ]; then
   echo "Backend directory does not exist!"
   exit 1
 fi
 
 # Navigate to frontend and install npm packages
-cd /home/ec2-user/majaliwasocialtest/frontend
+cd /home/ec2-user/frontend
 npm cache clean --force
 npm install
 
