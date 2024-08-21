60948d3a982281a866479d558aa042bb68526c74
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 11:43:01 +0300

    Update start_servers.sh---2

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Navigate to frontend and build the React app
+# Start Nginx service
+sudo systemctl start nginx
+
+# Start the React app using serve (if you're serving a static build)
 cd /home/ec2-user/majaliwasocialtest/frontend
-npm run build
+npm install -g serve
+serve -s build -l 3000 &
 
-# Navigate to backend and apply migrations
+# Start the Django server (or Gunicorn)
 cd /home/ec2-user/majaliwasocialtest/backend
-python3 manage.py migrate
-
-# Collect static files for Django
-python3 manage.py collectstatic --noinput
-
-# Start Gunicorn server for Django
 gunicorn --workers 3 --bind 0.0.0.0:8000 majaliwasocialtest.wsgi:application &
+
