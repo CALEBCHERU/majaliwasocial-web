6354b6ddfd560f82cc35ebc211cf60a3f7bef386
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 13:35:12 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the React frontend
-cd /home/ec2-user/frontend
+cd /frontend
 npm run build
 
 # Apply Django migrations and collect static files
-cd /home/ec2-user/backend
+cd /backend
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
