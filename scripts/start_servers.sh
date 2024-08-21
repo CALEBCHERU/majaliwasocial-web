a9a7506348cbf570c1e880451e1c0f6028169a09
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 11:41:39 +0300

    Update start_servers.sh--1

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Start the Gunicorn server for Django
-cd /home/ec2-user/backend
-gunicorn --workers 3 --bind 0.0.0.0:8000 majaliwasocialtest.wsgi:application &
+# Navigate to frontend and build the React app
+cd /home/ec2-user/majaliwasocialtest/frontend
+npm run build
 
-# Serve the React frontend using Nginx
-sudo systemctl start nginx
+# Navigate to backend and apply migrations
+cd /home/ec2-user/majaliwasocialtest/backend
+python3 manage.py migrate
+
+# Collect static files for Django
+python3 manage.py collectstatic --noinput
+
+# Start Gunicorn server for Django
+gunicorn --workers 3 --bind 0.0.0.0:8000 majaliwasocialtest.wsgi:application &
