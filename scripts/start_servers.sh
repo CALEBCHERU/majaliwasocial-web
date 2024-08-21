b8d7981221594f9a69a7390572666d174bdae5b9
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 13:35:48 +0300

    Update start_servers.sh

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -4,11 +4,11 @@
 sudo systemctl start nginx
 
 # Start the React app using serve (if you're serving a static build)
-cd /home/ec2-user/majaliwasocialtest/frontend
+cd /frontend
 npm install -g serve
 serve -s build -l 3000 &
 
 # Start the Django server (or Gunicorn)
-cd /home/ec2-user/majaliwasocialtest/backend
+cd /backend
 gunicorn --workers 3 --bind 0.0.0.0:8000 majaliwasocialtest.wsgi:application &
 
