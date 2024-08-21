9e801b69559497c6da5da72a2c7f37edb1e990e5
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 13:56:07 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Update package list and install Python 3 and pip
+sudo yum update -y
+sudo yum install -y python3
+
+# Install pip for Python 3
+sudo yum install -y python3-pip
+
 # Navigate to the backend directory
-cd /majaliwasocial/backend
+cd /backend
 
 # Install Python dependencies for the user
-pip install --user -r requirements.txt
+pip3 install --user -r requirements.txt
 
 # Run Django migrations
 python3 manage.py migrate
