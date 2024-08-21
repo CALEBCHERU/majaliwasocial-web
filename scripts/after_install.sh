31e8afab2e1529c3f597b1e3bed93c946f6b12b3
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 13:49:10 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
-# Navigate to the frontend directory
-cd /frontend
-
-# Install Node.js dependencies
-npm install
-
-# Build the React frontend
-npm run build
-
 # Navigate to the backend directory
-cd /backend
+cd /majaliwasocial/backend
 
-# Activate the virtual environment
-source /backend/venv/bin/activate
-
-# Install Python dependencies
-pip install -r requirements.txt
+# Install Python dependencies for the user
+pip install --user -r requirements.txt
 
 # Run Django migrations
-python manage.py migrate
+python3 manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
-
-# Deactivate the virtual environment
-deactivate
+python3 manage.py collectstatic --noinput
