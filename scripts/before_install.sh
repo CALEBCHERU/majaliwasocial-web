0d2cff261f5c63bd6bbcef67c71cee6b1b27b659
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 13:34:49 +0300

    Update before_install.sh------

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 
-# Stop the existing Node.js and Django services, if running
-sudo systemctl stop nginx || true
-sudo pkill -f node || true
-sudo pkill -f gunicorn || true
+# Listing contents of the majaliwasocial directory
+echo "Listing contents of /"
+ls -al /
 
-# Install necessary dependencies (Node.js, npm, Python, pip)
-sudo yum update -y
-sudo yum install -y nodejs npm
-sudo yum install -y python3-pip
-
-# Debug: List contents of the project directory and subdirectories
-echo "Listing contents of /home/ec2-user/"
-ls -la /home/ec2-user/
-
-echo "Listing contents of /home/ec2-user/frontend (if exists)"
-ls -la /home/ec2-user/frontend || echo "Frontend directory not found!"
-
-echo "Listing contents of /home/ec2-user/backend (if exists)"
-ls -la /home/ec2-user/backend || echo "Backend directory not found!"
-
-# Ensure the deployment directories exist
-if [ ! -d "/home/ec2-user/frontend" ]; then
+# Listing contents of the frontend directory
+if [ -d "/frontend" ]; then
+  echo "Listing contents of /frontend"
+  ls -al /frontend/
+else
   echo "Frontend directory does not exist!"
-  exit 1
 fi
 
-if [ ! -d "/home/ec2-user/backend" ]; then
+# Listing contents of the backend directory
+if [ -d "/backend" ]; then
+  echo "Listing contents of /backend"
+  ls -al /backend/
+else
   echo "Backend directory does not exist!"
-  exit 1
 fi
-
-# Navigate to frontend and install npm packages
-cd /home/ec2-user/frontend
-npm cache clean --force
-npm install
-
-# Navigate to backend and install Python packages
-cd /home/ec2-user/majaliwasocialtest/backend
-if [ ! -f "requirements.txt" ]; then
-  echo "requirements.txt not found!"
-  exit 1
-fi
-pip3 install -r requirements.txt
