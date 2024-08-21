5e21a95daa94834538a33fd259fbf5957568068d
CALEBCHERU <159821047+CALEBCHERU@users.noreply.github.com>
2024-08-21 11:43:24 +0300

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Check if Gunicorn is running
-if pgrep -f gunicorn > /dev/null
-then
-    echo "Gunicorn is running"
-else
-    echo "Gunicorn is NOT running"
-    exit 1
+# Check if the React app is running
+curl -Is http://localhost:3000 | head -n 1 | grep 200
+if [ $? -ne 0 ]; then
+  echo "React app is not running on port 3000"
+  exit 1
 fi
 
-# Check if Nginx is running
-if systemctl status nginx | grep "active (running)" > /dev/null
-then
-    echo "Nginx is running"
-else
-    echo "Nginx is NOT running"
-    exit 1
+# Check if the Django app is running
+curl -Is http://localhost:8000 | head -n 1 | grep 200
+if [ $? -ne 0 ]; then
+  echo "Django app is not running on port 8000"
+  exit 1
 fi
+
+echo "Both React and Django apps are running successfully."
